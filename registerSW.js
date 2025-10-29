4bda44194cfc0171d251cf8356776a1af75373df
9 <chugaev.aa99@gmail.com>
2025-10-29 18:48:38 +0300

    add PWA

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vue-frontendmentor-weather-now/sw.js', { scope: '/vue-frontendmentor-weather-now/' })})}
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +11,43 @@ export default defineConfig({
   build: {
     outDir: "./",
   },
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Weather Now",
+        short_name: "Weather Now",
+        start_url: "/vue-frontendmentor-weather-now/",
+        display: "standalone",
+        background_color: "#23a4ec",
+        theme_color: "#23a4ec",
+        icons: [
+          {
+            src: "/vue-frontendmentor-weather-now/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/vue-frontendmentor-weather-now/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/vue-frontendmentor-weather-now/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/vue-frontendmentor-weather-now/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
